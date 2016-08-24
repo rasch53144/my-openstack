383142d2a3eb0a4d7b3e5ff12693b065552fd23b
Danny Rasch <rasch53144@gmail.com>
2016-08-24 14:54:43 -0500

    last assignment, week 2

---
+++ cisco_find.py
@@ -0,0 +1,8 @@
+cisco_ios = "Cisco IOS Software, C880 Software (C880DATA-UNIVERSALK9-M), Version 15.0(1)M4, RELEASE SOFTWARE (fc1)"
+
+a = cisco_ios.split(" ")
+cisco_ios = a[7]
+
+print cisco_ios
+
+# for loop could parse the list
